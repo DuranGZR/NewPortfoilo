3f058cb295df6c3488658282303823d8e85736eb
Duran Gezer <durangezer2004@gmail.com>
2026-01-13 17:19:52 +0300

    config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,8 +3,10 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default withNextIntl(nextConfig);
+export default withNextIntl(nextConfig as any);
